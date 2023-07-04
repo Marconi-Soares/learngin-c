f3f1199398d5618628e27b4634d75e05bb203e92
Marconi S. de Jesus <marconi.developer@outlook.com.br>
2023-07-04 13:01:37 -0300

    strings

---
+++ strings/strings.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <string.h>
+
+int main(void){
+    char string1[50] = "Marconi Soares";
+    char string2[] = "Marconi SoarEs";
+    
+    // strcpy(string1, string2);
+    // strncat(string1, string2, 2);
+    // memset(string1, '?', sizeof( string1 ));
+    // short res = strcasecmp(string1, string2);
+    // printf("%hd\n", res);
+    return 0;
+}
